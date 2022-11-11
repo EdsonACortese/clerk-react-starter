76b8e00186c03e4f5ba26e1012f7bafcb1f1854f
EdsonACortese <99457481+EdsonACortese@users.noreply.github.com>
2022-11-11 00:25:34 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles/index.css";
+import App from "./App";
+import Layout from "./components/Layout";
+import NoMatch from "./components/NoMatch";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ClerkProvider frontendApi={process.env.REACT_APP_CLERK_FRONTEND_API}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+          </Route>
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
